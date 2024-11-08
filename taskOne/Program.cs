2e47f2f594f72dd08248b0285726cfcf46678244
eman ayman <emanayman835@yahoo.com>
2024-11-08 20:44:24 +0200

    first commit

---
+++ taskOne/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace taskOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersmall;
+            int numberlarge;
+            int priceSmall = 25;
+            int pricelarge = 35;
+            double taxRate = 0.06;
+            Console.WriteLine(" Welcome for carpet cleaning service");
+            Console.WriteLine("Number of small carpets:");
+            numbersmall = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number of large carpets:");
+            numberlarge = Convert.ToInt32(Console.ReadLine());
+            int coast = (numbersmall * priceSmall) + (numberlarge * pricelarge);
+            double tax = coast * taxRate;
+            double total = coast + tax;
+            Console.WriteLine(total);
+            Console.WriteLine("This estimate is valid for 30 days");
+        }
+    }
+}
